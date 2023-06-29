de41ded31e180446efc615262e4e607b6c62ed2c
tweacktech <daridatongs@gmail.com>
2023-06-29 02:53:32 +0100

    sssssss

--- app/Http/Controllers/Gallerycontroller.php
+++ app/Http/Controllers/Gallerycontroller.php
@@ -4,87 +4,146 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Gallery;
+use Carbon\Carbon;
+use Validator;
+use DB;
+use Auth;
 
 class Gallerycontroller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+ public function __construct()
     {
-        return $data=Gallery::all();
+        $this->middleware('auth');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-       
-        return $data=Gallery::create($request->all());
-    }
+public function indexGallery(){
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-     
-        return $data=Gallery::findorFail($id);
-    }
+  $data =Gallery::latest()->paginate(6);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
+  return view('gallery.index',compact('data'));
+}
 
-    }
+public function addGallery(Request $req){
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-         $data=Gallery::findorFail($id);
-        return $data->update();
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-         $data=Gallery::findorFail($id);
-       return $data->delete();
+      $validator = Validator::make($req->all(), [
+            'Name' => 'required|string|max:255',
+            'Image' => 'required|Image',
+            
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        if ($req->file('Image')=="") {
+            
+        }else{
+        $file = $req->file('Image');
+    $file_name = time() . $file->getClientOriginalName();
+    $file->move(public_path('gallery/'), $file_name);
+        // Create a new Gallery with validated data
+        $Gallery = new Gallery();
+        $Gallery->Name = $req->input('Name');
+        $Gallery->Image = $file_name;
+        $Gallery->save();
+  return redirect()->back();}
+}
+
+public function deleteGallery($id) {
+    $Gallery = Gallery::find($id);
+    $Gallery->delete();
+    
+    return redirect()->back()->with('success', 'Gallery deleted successfully.');
+}
+
+
+
+
+public function unhideGallery(Request $req, $id)
+  {
+    $update = DB::table('galleries')->where('id', $id)->update(['status' => '1']);
+    if ($update) {
+      
+      return redirect()->back();
     }
+    return redirect()->back();
+  }
+
+  public function hideGallery(Request $req, $id)
+  {
+    //$data = ['status' => 0];
+    $update = DB::table('galleries')->where('id', $id)->update(['status' => '0']);
+    if ($update) {
+      
+      return redirect()->back();
+    }
+    return redirect()->back();
+  }
+
+public function editGallery(Request $req, $id)
+  {
+    
+    $update =Gallery::find( $id);
+
+    
+    return view('gallery.show',compact('update'));
+  }
+
+
+public function updateGallery(Request $request, $id) 
+{
+
+  if ($request->file('Image')!="" && $request->file('Audio')!="") {
+     $file = $request->file('Image');
+    $file_name = time() . $file->getClientOriginalName();
+    $file->move(public_path('gallery/'), $file_name);
+
+     $files = $request->file('Audio');
+    $file_names = time() . $files->getClientOriginalName();
+    $files->move(public_path('sermon/'), $file_names);
+
+    $Gallery = Gallery::find($id);
+   $Gallery->Name = $request->input('Name');
+   
+    $Gallery->Image = $file_name;
+    $Gallery->save();
+    return redirect()->back()->with('success', 'Gallery updated successfully.');
+
+  }elseif ($request->file('Image')!="") {
+    $file = $request->file('Image');
+    $file_name = time() . $file->getClientOriginalName();
+    $file->move(public_path('sermon/'), $file_name);
+
+    $Gallery = Gallery::find($id);
+   $Gallery->Name = $request->input('Name');
+   
+    $Gallery->Image = $file_name;
+    $Gallery->save();
+    return redirect()->back()->with('success', 'Gallery updated successfully.');
+
+  }elseif($request->file('Audio')!=""){
+
+     $files = $request->file('Audio');
+    $file_names = time() . $files->getClientOriginalName();
+    $files->move(public_path('sermon/'), $file_names);
+
+    $Gallery = Gallery::find($id);
+   $Gallery->Name = $request->input('Name');
+   
+    $Gallery->save();
+    return redirect()->back()->with('success', 'Gallery updated successfully.');
+
+  }else{
+
+   $Gallery = Gallery::find($id);
+   $Gallery->Name = $request->input('Name');
+   
+    $Gallery->save();
+    return redirect()->back()->with('success', 'Gallery updated successfully.');
+
+  }
+
+}
 }
